c7e370c57364c95e1f3a0195eec7161e89a14b16
Saad Masood <sunnygdr@gmail.com>
2026-08-14 00:35:30 +0500

    Export canonical materials API

---
+++ pvl/materials/__init__.py
@@ -0,0 +1,4 @@
+from pvl.materials.library import MaterialLibrary, load_builtin_material_library
+from pvl.materials.models import MaterialDefinition
+
+__all__ = ["MaterialDefinition", "MaterialLibrary", "load_builtin_material_library"]
