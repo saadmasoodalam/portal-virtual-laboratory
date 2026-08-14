0c12c173ababa8557a3df7624322f16e36213763
Saad Masood <sunnygdr@gmail.com>
2026-08-14 05:46:06 +0500

    Load DC planner styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './experiment.css';
+import './dcPlan.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
